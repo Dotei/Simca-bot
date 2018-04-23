d1aac0e5839fbfa94acc306f578878119982505f
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 00:18:00 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -28,6 +28,26 @@ bot.on("message", message => {
         console.log('Ping Pong !');
     }
    
+    if (message.content.startsWitch(prefix + "sondage")){
+	if(message.author.id == "437991432419016714"){
+		let args = message.content.split(" ").slice(1);
+		var embed = new Discord.RichEmbed()
+			.setDescription("Sondage")
+			.addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+			.setColor("0xB40404")
+			.setTimestamp()
+		message.guild.channels.find("namee", "sondage").sendEmbed(embed)
+		.then(function (message){ 
+			message.react("✔")
+			message.react("✖")
+
+		}).catch(function(){
+		});	
+		}else{
+			return message.reply("tu n'as pas la permission") 
+}}})
+
+    
 });
 
 bot.login(process.env.TOKEN);
