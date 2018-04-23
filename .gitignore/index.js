810064e63d5e1f0848be63729bd82566a496c16f
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-23 22:06:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,15 +14,9 @@ bot.on("message", message => {
     if (message.content === prefix + "help"){  
         message.channel.sendMessage("Liste des commandes: \n -!help");
     }
-    {
+
     if (message.content === "ping"){  
         message.reply("Pong !");
         console.log('Ping Pong !');
-    }
-    {
-
-    if (message.content === "ching"){  
-        message.reply("Chang !");
-        console.log('Ching Chang !');
     }
 });
