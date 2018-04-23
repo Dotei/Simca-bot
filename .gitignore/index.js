26aa836b8a9d158e64d6d0df687a2f7e4bc6589a
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-23 21:53:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -20,20 +20,5 @@ bot.on("message", message => {
         console.log('Ping Pong !');
     }
     
-    }
-if (message.content.startsWith('h!8ball')) {
-    if ( message.content.endsWith('?')) {
-      var answers = [
-      '8ball: Peut être', '8ball: Lol non.', '8ball: Je l'espère.', '8ball: Dans tes rêves.',
-      '8ball: Il y a de fortes chances.', '8ball: Très probable', '8ball: Je pense que Oui', '8ball: J'espère que non',
-      '8ball: J'espere.', '8ball: Jamais!', '8ball: NOOPE!', '8ball: Ahaha! Vraiment?!?', '8ball: Pfft.',
-      '8ball: Désoler, mais non !.', '8ball: yeaaa.', '8ball: Absolument hors de question.', '8ball: ehhhhhh, je ne sais pas.',
-      '8ball: L'avenir est incertain.', '8ball: Je préfère ne pas dire.', '8ball: On s'en fout?',
-      '8ball: Possible.', '8ball: Jamais! jamais! jamais!', '8ball: Il y a une petite chance.', '8ball: Oui!'];
-      var answer = answers[Math.floor(Math.random() * answers.length)];
-    } else {
-      message.channel.sendMessage('Ce n'est pas une question valide. S'il vous plaît ajouter à votre question "?"!')
-    }
-  message.channel.sendMessage(answer);
-}
+
 });
