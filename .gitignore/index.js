3c0e7bef6a5c85b680459ec60b30fcffefdfe0fd
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 15:12:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
-const TOKEN = "NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s"
 const PREFIX ="!"
 
 var bot = new Discord.Client();
 
-bot.login(TOKEN);
+client.login(process.env.TOKEN);
+
 
 bot.on("ready", function() {
     console.log("Ready");
