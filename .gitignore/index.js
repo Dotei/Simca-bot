8a5adfff9454310fdb1a9a4109e4d3ffd2f96e6a
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 15:44:47 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,31 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-bot.on('ready', function () {
-  console.log("Je suis connecté !")
-})
+var prefix = ("!")
+
+bot.on("ready", function() {
+    bot.user.setGame("Command: !help");
+    console.log("connected");
+});
+
+bot.on("message", message => {
+    if (message.content === prefix + "help"){  
+        message.channel.sendMessage("Liste des commandes: \n -!help");
+    }
+
+    if (message.content === "ping"){  
+        message.reply("Pong !");
+        console.log('Ping Pong!');
+    }   
+        if (message.content === prefix + "info"){  
+        message.reply("J'ai été coder par Dotei !");
+        console.log('info bot !');
+    }
+        if (message.content === "ching"){  
+        message.reply("Chang!");
+        console.log('Ching Chang!');
+    }   
+
+});
 
 client.login(process.env.TOKEN);
