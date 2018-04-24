5e8a2a6fe3ec58ce7c348c213f24bad7b9d6ca50
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 16:54:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
 var bot = new Discord.Client();
-
-var PREFIX = "!"
+bot.login(process.env.TOKEN);
+var prefix = ("!");
 
 bot.on('message', message => {
-    if(message.content === PREFIX + "infodiscord") {
+
+    if(message.content === prefix + "infodiscord") {
         var embed = new Discord.RichEmbed()
         .setDescription("Information du Discord")
         .addField("Nom du discord", message.guild.name)
@@ -18,15 +19,15 @@ bot.on('message', message => {
 
     if (message.content.startsWitch(PREFIX + "sondage")) {
         if(message.author.id == "238937155680993281"){ 
-            let args = message.content.split(" ").slice(1);
-            let thingToEcho = args.join(" ")
-            var embed = new Discord.RichEmbed()
+        let args = message.content.split(" ").slice(1);
+        let thingToEcho = args.join(" ")
+        var embed = new Discord.RichEmbed()
             .setDescription("Sondage")
             .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
             .setColor("0xB40404")
             .setTimestamp()
         message.guild.channels.find("name", "sondage"). sendEmbed(embed)
-        .then(function(message) { 
+        .then(function (message) { 
             message.react("✅")
             message.react("✕")
         }).catch(function() { 
@@ -34,5 +35,3 @@ bot.on('message', message => {
         }else{ 
             return message.reply("tu n'as pas la permission.")
 }}})
-
-bot.login(process.env.TOKEN);
