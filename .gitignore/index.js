81d634d18e7f6b6fe08f35f2dc21c8cbb7d76460
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 16:42:23 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,36 +1,36 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
-  
-var PREFIX = ("!")
+var bot = new Discord.Client();
+bot.on('message', message => {
 
+    if(message.content === prefix + "infodiscord") {
+        var embed = new Discord.RichEmbed()
+        .setDescription("Information du Discord")
+        .addField("Nom du discord", message.guild.name)
+        .addField("Crée le", message.guild.createdAt)
+        .addField("Tu as rejoin le", message.member.joinedAt)
+        .addField("Utilisateurs sur le discord", message.guild.memberCount)
+        .setColor("0x0000FF")
+    message.channel.sendEmbed(embed)
 
-bot.on("ready", function() {
-    bot.user.setGame("Command: !help");
-    console.log("connected");
-})
-
-bot.on("message", message => {
-    if (message.content === prefix + "help"){  
-        message.channel.sendMessage("Liste des commandes: \n -!help");
     }
 
-    if (message.content === "ping"){  
-        message.reply("Pong !");
-        console.log('Ping Pong!');
-    }   
-        if (message.content === prefix + "info"){  
-        message.reply("J'ai été coder par Dotei !");
-        console.log('info bot !');
-    }
-        if (message.content === "ching"){  
-        message.reply("Chang!");
-        console.log('Ching Chang!');
-    }
-
-        if (message.content === PREFIX + "avatar") { 
-        message.reply(message.author.avatarURL);
-    });
-
-
+    if (message.content.startsWitch(prefix + "sondage")) {
+        if(message.author.id == "238937155680993281"){ 
+            let args = message.content.split(" ").slice(1);
+            let thingToEcho = args.join(" ")
+            var embed = new Discord.RichEmbed()
+            .setDescription("Sondage")
+            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+            .setColor("0xB40404")
+            .setTimestamp()
+        message.guild.channels.find("name", "sondage"). sendEmbed(embed)
+        .then(function(message) { 
+            message.react("✅")
+            message.react("✕")
+        }).catch(function() { 
+        });
+        }else{ 
+            return message.reply("tu n'as pas la permission.")
+}}})
 
 bot.login(process.env.TOKEN);
