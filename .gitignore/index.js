b6bcbf55d8fccc3c1bfd13e97ee0ffed79ce14b3
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 16:29:35 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -28,4 +28,36 @@ bot.on("message", message => {
     }
   
 });  
+
+bot.on("message", function(message){
+    if (message.author.equals(bot.user)) return;
+
+    if (message.content == "bonjour"){
+        message.channel.sendMessage("Hey");
+    }
+
+      
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split (" ")
+
+    switch (args[0]) {
+        case "ping":
+            message.channel.sendMessage("Pong!");
+            break;
+        case "cry":
+            var embed = new Discord.RichEmbed()
+                .setDescription(`${message.author.tag} is sad`)
+                .setImage("https://cdn.weeb.sh/images/ryVBX8mw-.gif")
+            message.channel.sendEmbed(embed);
+            break;
+           
+        default:
+            message.channel.sendMessage("Invalid command")
+
+        
+    }  
+     
+});
+
 bot.login(process.env.TOKEN);
