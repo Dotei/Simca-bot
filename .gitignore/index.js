646a7b09e565d44ce05de28ef263d4e867f10456
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 23:11:58 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -42,6 +42,19 @@ bot.on("message", (message) => {
     
         return message.channel.send(serverembed);
       }
+    
+     if (message.content === PREFIX + "botinfo"){
+     
+        let bicon = bot.user.displayAvatarURL;
+        let botembed = new Discord.RichEmbed()
+        .setDescription("Information du bot")
+        .setColor("#15f153")
+        .setThumbnail(bicon)
+        .addField("Nom du bot", bot.user.username)
+        .addField("Crée le", bot.user.createdAt);
+    
+        return message.channel.send(botembed);
+      }
 
 
       
