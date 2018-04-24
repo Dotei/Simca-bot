5b5e528823b5eb0ab4c9c4cde1c109d4eb73af9e
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 16:57:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,33 +1,17 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
-  
-var prefix = ("!")
 
+const TOKEN = "NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s"
+const PREFIX ="!"
+
+var bot = new Discord.Client();
+
+bot.login(process.env.TOKEN);
 
 bot.on("ready", function() {
-    bot.user.setGame("Command: !help");
-    console.log("connected");
-})
+    console.log("Ready");
+});
 
-bot.on("message", message => {
-    if (message.content === prefix + "help"){  
-        message.channel.sendMessage("Liste des commandes: \n -!help");
-    }
-
-    if (message.content === "ping"){  
-        message.reply("Pong !");
-        console.log('Ping Pong!');
-    }   
-        if (message.content === prefix + "info"){  
-        message.reply("J'ai été coder par Dotei !");
-        console.log('info bot !');
-    }
-        if (message.content === "ching"){  
-        message.reply("Chang!");
-        console.log('Ching Chang!');
-    }
-  
-  bot.on("message", function(message){
+bot.on("message", function(message){
     if (message.author.equals(bot.user)) return;
 
     if (message.content == "bonjour"){
@@ -56,7 +40,4 @@ bot.on("message", message => {
         
     }  
      
-
-});  
-
-bot.login(process.env.TOKEN);
+});
