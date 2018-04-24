4cca2d0dc5aa39e57988bcebc5593485bd1b9d0b
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 16:24:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -39,26 +39,6 @@ bot.on("message", message => {
         .setColor("0x0000FF")
     message.channel.sendEmbed(embed)
 
-    }
-
-    if (message.content.startsWitch(prefix + "sondage")) {
-        if(message.author.id == "238937155680993281"){ 
-            let args = message.content.split(" ").slice(1);
-            let thingToEcho = args.join(" ")
-            var embed = new Discord.RichEmbed()
-            .setDescription("Sondage")
-            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-            .setColor("0xB40404")
-            .setTimestamp()
-        message.guild.channels.find("name", "sondage"). sendEmbed(embed)
-        .then(function(message) { 
-            message.react("✅")
-            message.react("✕")
-        }).catch(function() { 
-        });
-        }else{ 
-            return message.reply("tu n'as pas la permission.")
-}}})
   
-  
+});  
 bot.login(process.env.TOKEN);
