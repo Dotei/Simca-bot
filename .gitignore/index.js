6f3ce9bfc3e1ae39962bdbaae4a81526db14ed80
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 15:35:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,35 +1,58 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-var bot = new Discord.Client();
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
-bot.login(TOKEN);
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
 
-var prefix = ("!")
+bot.login('TOKEN')
 
-bot.on("ready", function() {
-    bot.user.setGame("Command: !help");
-    console.log("connected")
-});
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong !')
+  }
+})
+
+bot.on('guildMemberAdd', member => {
+  member.createDM().then(channel => {
+    return channel.send('Bienvenue sur mon serveur ' + member.displayName)
+  }).catch(console.error)
+  // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
+})
+
+bot.on('message', message => {
+
+  if (message.content.startsWith('!play')) {
+    // On récupère le premier channel audio du serveur
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    // On récupère les arguments de la commande 
+    // il faudrait utiliser une expression régulière pour valider le lien youtube
+    let args = message.content.split(' ')
+    // On rejoint le channel audio
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        // On démarre un stream à partir de la vidéo youtube
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        // On envoie le stream au channel audio
+        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+})
 
 
-client.on("message", message => {
-    if (message.content === prefix + "help"){  
-        message.channel.sendMessage("Liste des commandes: \n -!help");
-    }
 
-    if (message.content === "ping"){  
-        message.reply("Pong !");
-        console.log('Ping Pong!');
-    }   
-        if (message.content === prefix + "info"){  
-        message.reply("J'ai été coder par Dotei !");
-        console.log('info bot !');
-    }
-        if (message.content === "ching"){  
-        message.reply("Chang!");
-        console.log('Ching Chang!');
-    }   
-
-});
 
 client.login(process.env.TOKEN);
