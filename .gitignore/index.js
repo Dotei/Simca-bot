d9ca785bb0b78b6cc9202f1597262ee191806ac8
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 15:02:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,6 +11,51 @@ bot.on("ready", function() {
 
 client.login(process.env.TOKEN);
 
+const Discord = require("discord.js");
+
+const TOKEN = "NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s"
+const PREFIX ="!"
+
+var bot = new Discord.Client();
+
+bot.login(TOKEN);
+
+bot.on("ready", function() {
+    console.log("Ready");
+});
+
+bot.on("message", function(message){
+    if (message.author.equals(bot.user)) return;
+
+    if (message.content == "bonjour"){
+        message.channel.sendMessage("Hey");
+    }
+
+      
+    if (!message.content.startsWith(PREFIX)) return;
+
+    var args = message.content.substring(PREFIX.length).split (" ")
+
+    switch (args[0]) {
+        case "ping":
+            message.channel.sendMessage("Pong!");
+            break;
+        case "cry":
+            var embed = new Discord.RichEmbed()
+                .setDescription(`${message.author.tag} is sad`)
+                .setImage("https://cdn.weeb.sh/images/ryVBX8mw-.gif")
+            message.channel.sendEmbed(embed);
+            break;
+           
+        default:
+            message.channel.sendMessage("Invalid command")
+
+        
+    }  
+     
+});
+
+
 client.on("message", message => {
     if (message.content === prefix + "help"){  
         message.channel.sendMessage("Liste des commandes: \n -!help");
