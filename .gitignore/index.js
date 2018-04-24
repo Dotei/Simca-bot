75714e41d73162128f8c48604646fadc47bcaf2b
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-24 15:13:16 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,43 +1,33 @@
 const Discord = require("discord.js");
+const client = new Discord.Client();
 
-const PREFIX ="!"
 
-var bot = new Discord.Client();
+var prefix = ("!")
+
+bot.on("ready", function() {
+    bot.user.setGame("Command: !help");
+    console.log("connected")
+});
 
 client.login(process.env.TOKEN);
 
-
-bot.on("ready", function() {
-    console.log("Ready");
-});
-
-bot.on("message", function(message){
-    if (message.author.equals(bot.user)) return;
-
-    if (message.content == "bonjour"){
-        message.channel.sendMessage("Hey");
+client.on("message", message => {
+    if (message.content === prefix + "help"){  
+        message.channel.sendMessage("Liste des commandes: \n -!help");
     }
 
-      
-    if (!message.content.startsWith(PREFIX)) return;
+    if (message.content === "ping"){  
+        message.reply("Pong !");
+        console.log('Ping Pong!');
+    }   
+        if (message.content === prefix + "info"){  
+        message.reply("J'ai été coder par Dotei !");
+        console.log('info bot !');
+    }
+        if (message.content === "ching"){  
+        message.reply("Chang!");
+        console.log('Ching Chang!');
+    }   
 
-    var args = message.content.substring(PREFIX.length).split (" ")
-
-    switch (args[0]) {
-        case "ping":
-            message.channel.sendMessage("Pong!");
-            break;
-        case "cry":
-            var embed = new Discord.RichEmbed()
-                .setDescription(`${message.author.tag} is sad`)
-                .setImage("https://cdn.weeb.sh/images/ryVBX8mw-.gif")
-            message.channel.sendEmbed(embed);
-            break;
-           
-        default:
-            message.channel.sendMessage("Invalid command")
-
-        
-    }  
-     
+    
 });
