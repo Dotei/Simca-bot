025f35ebbd01ed550e04db8e1e3834d381e269ff
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-25 21:44:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,10 @@
 const Discord = require("discord.js");
-
-const TOKEN = "NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s"
 const PREFIX ="!"
 const botconfig = require("./botconfig.json");
-
-
 var bot = new Discord.Client();
 
 bot.login(process.env.TOKEN);
-bot.login("NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s");
+
 
 bot.on("ready", function() {
     console.log("Ready");
@@ -55,9 +51,7 @@ bot.on("message", (message) => {
     
         return message.channel.send(botembed);
       }
-
-
-      
+	
     if (!message.content.startsWith(PREFIX)) return;
 
     var args = message.content.substring(PREFIX.length).split (" ")
