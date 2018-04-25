4d233b4e24e3b5d38311af1a856ce3c32e268c29
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-25 20:27:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,92 +1,22 @@
 const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-const TOKEN = "NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s"
-const PREFIX ="!"
-const botconfig = require("./botconfig.json");
-
-
-var bot = new Discord.Client();
-
-bot.login(process.env.TOKEN);
+var prefix = ("!")
 
 bot.on("ready", function() {
-    console.log("Ready");
+    bot.user.setGame("Command: !help");
+    console.log("connected")
 });
 
+bot.login('process.env.TOKEN');
 
-bot.on("message", function(message){
-        if (message.author.equals(bot.user)) return;
-    })
-
-    bot.on("message", (message) => {
-        if (message.content === "bonjour"){
-        message.reply("Hey");
-
-  }
-       
-      
-        if (message.content === PREFIX + "info"){  
-        message.reply("```Je suis toujours en développement, coder par Dotei ! ```");
-        console.log('info bot !');
+bot.on("message", message => {
+    if (message.content === prefix + "help"){  
+        message.channel.sendMessage("Liste des commandes: \n -!help");
     }
-    
-        if (message.content === PREFIX + "serverinfo"){
-    
-        let sicon = message.guild.iconURL;
-        let serverembed = new Discord.RichEmbed()
-        .setDescription("Server Information")
-        .setColor("#15f153")
-        .setThumbnail(sicon)
-        .addField("Nom du serveur", message.guild.name)
-        .addField("Crée le", message.guild.createdAt)
-        .addField("Rejoin le", message.member.joinedAt)
-        .addField("Nombre de membres", message.guild.memberCount);
-    
-        return message.channel.send(serverembed);
-      }
-	
-    
 
-     if (message.content === PREFIX + "botinfo"){
-     
-        let bicon = bot.user.displayAvatarURL;
-        let botembed = new Discord.RichEmbed()
-        .setDescription("Information du bot")
-        .setColor("#15f153")
-        .setThumbnail(bicon)
-        .addField("Nom du bot", bot.user.username)
-        .addField("Crée le", bot.user.createdAt);
-    
-        return message.channel.send(botembed);
-      }
-
-
-      
-    if (!message.content.startsWith(PREFIX)) return;
-
-    var args = message.content.substring(PREFIX.length).split (" ")
-
-    switch (args[0]) {
-        case "ping":
-            message.channel.sendMessage("Pong!");
-            break;
-        case "cry":
-            var embed = new Discord.RichEmbed()
-                .setTitle(`${message.author.tag} is sad`)
-                .setImage("https://cdn.weeb.sh/images/ryVBX8mw-.gif")
-            message.channel.sendEmbed(embed);
-            break;
-         case "punch":
-            var embed = new Discord.RichEmbed()
-                .setTitle(`Simca punches ${message.author.tag} `)
-                .setImage("http://imgur.com/6wKJVHy.gif")
-            message.channel.sendEmbed(embed);
-            break;
-            case "hug":
-                var embed = new Discord.RichEmbed()
-                    .setTitle(`Simca hugs ${message.author.tag} `)
-                    .setImage("http://i.imgur.com/SUdqF9w.gif")
-                message.channel.sendEmbed(embed);
-            break;    
-         }     
+    if (message.content === "ping"){  
+        message.reply("Pong !");
+        console.log('Ping Pong !');
+    }
 });
