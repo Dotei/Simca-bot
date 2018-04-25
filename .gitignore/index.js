7f072ca00584ae40d64b5df3919fe0d59bc75d43
Dotei <36529280+Dotei@users.noreply.github.com>
2018-04-25 21:33:53 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,19 +1,37 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client();
 
-var prefix = ("!")
+const TOKEN = "NDM4MjkyNjgzODcwNTY4NDQ4.DcCfZQ.wJjozX0tXzb3xbSZ1bxh_rkyn_s"
+const PREFIX ="!"
+const botconfig = require("./botconfig.json");
+
+
+var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("Command: !help");
-    console.log("connected")
+    console.log("Ready");
 });
 
 bot.login('process.env.TOKEN');
 
 bot.on("message", message => {
-    if (message.content === prefix + "help"){  
-        message.channel.sendMessage("Liste des commandes: \n -!help");
+    if (message.content === "bonjour"){
+        message.reply("Hey");
+
     }
+    if (message.content === PREFIX + "serverinfo"){
+    
+        let sicon = message.guild.iconURL;
+        let serverembed = new Discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#15f153")
+        .setThumbnail(sicon)
+        .addField("Nom du serveur", message.guild.name)
+        .addField("Crée le", message.guild.createdAt)
+        .addField("Rejoin le", message.member.joinedAt)
+        .addField("Nombre de membres", message.guild.memberCount);
+    
+        return message.channel.send(serverembed);
+      }
 
     if (message.content === "ping"){  
         message.reply("Pong !");
